23ca1403125eeb731b4a2eef6534e46ed648be70
Valss22 <valsshokorov@gmail.com>
2023-02-25 01:34:41 +0600

    handling validation req body error added

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,30 +3,32 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Services\UserService;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
     protected $userService;
-
     public function __construct(UserService $userService)
     {
         $this->userService = $userService;
     }
 
-    public function register(Request $request): JsonResponse
+    public function register(Request $request)
     {   
-        $validatedData = $request->validate([
-            'name' => 'required|max:255',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:3|confirmed',
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|max:255',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|min:3',
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        }
 
         $user = $this->userService->createUser($validatedData);
-
         $token = $user->createToken('auth_token')->plainTextToken;
-
+        
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
