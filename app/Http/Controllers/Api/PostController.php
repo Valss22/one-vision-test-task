e50583837e68d971575592982e9b82eead190681
Valss22 <valsshokorov@gmail.com>
2023-02-25 18:16:18 +0600

    type hint to contoller added

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 use App\Services\PostService;
 use App\Http\Controllers\Controller;
@@ -17,27 +20,27 @@ class PostController extends Controller
         $this->postService = $postService;
     }
 
-    public function index(Request $request)
+    public function index(Request $request): LengthAwarePaginator
     {   
         return $this->postService->getPostList($request); 
     }
 
-    public function store(Request $request)
+    public function store(Request $request): HttpException|Post
     {
         return $this->postService->createPost($request); 
     }
 
-    public function show(int $id)
+    public function show(int $id): Post
     {
         return $this->postService->getPostById($id);
     }
 
-    public function update(Request $request, int $id)
+    public function update(Request $request, int $id): HttpException|Post
     {
         return $this->postService->updatePost($request, $id);
     }
 
-    public function destroy(int $id)
+    public function destroy(int $id): JsonResponse
     {
         return $this->postService->deletePost($id);
     }
